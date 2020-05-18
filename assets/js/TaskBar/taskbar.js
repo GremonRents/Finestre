2d90e6297c7f8cc78fbc25de8690be66a49dbec7
Giuseppe Patané <patanegiuseppe7@gmail.com>
2020-05-18 02:08:16 +0200

    Add upload file app

--- assets/js/TaskBar/taskbar.js
+++ assets/js/TaskBar/taskbar.js
@@ -30,6 +30,28 @@ function newShell(event){
 }
 
 function newUpload(event){
+    let id = freeId();
+    $("#background_container").append(
+        `<div id="${id}" class="draggable_window window_container">
+            <div class="title_bar draggable_bar">
+                <div class="title_section">
+                    <div class="icon_container"><img src="./assets/images/title_bar/upload.svg" class="icon"></div>
+                </div>
+                <div class="icons_section">
+                    <div class="icon_container minimize"><img src="./assets/images/title_bar/minimize.svg" class ="icon" alt="minimize"></div>
+                    <div class="icon_container maximize"><img src="./assets/images/title_bar/maximize.svg" class ="icon" alt = "maximize"></div>
+                    <div class="icon_container close"><img src="./assets/images/title_bar/close.svg" class = "icon" alt="close"></div>
+                </div>
+            </div>
+            <div class="window_content">
+                <label class="upload_section">
+                    <input type="file" name="upload">
+                    <div class="upload_container"><img src="./assets/images/apps/upload_button.svg" alt="upload"></div>
+                </label>
+            </div>
+        </div>`);
+
+        windows[id] = new Window(id);
 
 }
 
